fix training loop (masking)

nocap/models.py
@@ -8,7 +8,7 @@ from typing import List, Tuple, Dict
 
 clip_model_dict = {
     'vision_model': CLIPVisionModel.from_pretrained('openai/clip-vit-base-patch32'),
-    'text_model': CLIPTextModel.from_pretrained('openai/clip-vit-base-patch32'),
+    'text_model': CLIPTextModel.from_pretrained('openai/clip-vit-base-patch32').text_model.embeddings,
     'image_processor': AutoProcessor.from_pretrained('openai/clip-vit-base-patch32', use_fast=True),
     'text_tokeniser': AutoTokenizer.from_pretrained('openai/clip-vit-base-patch32'),
     'vision_hidden_dim': 768,
@@ -16,7 +16,7 @@ clip_model_dict = {
 }
 
 
-def get_text_inputs_and_targets(tokenised_text:Dict, target_id:int, replacement_id:int) -> Tuple[Dict, torch.Tensor]:
+def get_text_inputs_and_targets(tokenised_text:torch.Tensor, attention_mask, target_id:int, replacement_id:int) -> Tuple[Dict, torch.Tensor]:
     """ processed_text is the CLIP-tokenised text
     This function produces the inputs to the model and the targets for evaluation
     on the auto-regressive task.
@@ -24,27 +24,46 @@ def get_text_inputs_and_targets(tokenised_text:Dict, target_id:int, replacement_
     """
 
     # Want the model to predict all but the last token and so we slice until it
-    text_inputs = deepcopy(tokenised_text)
-    text_inputs['input_ids'] = text_inputs['input_ids'][:, :-1]
-    text_inputs['attention_mask'] = text_inputs['attention_mask'][:, :-1]
+    tokenised_text = deepcopy(tokenised_text)
+    text_inputs = tokenised_text[:, :-1]
+    attention_mask = deepcopy(attention_mask)[:, :-1]
 
     # Targets are all but the first token (don't predict <bos>)
     text_targets = process_padding(
-        tokenised_text['input_ids'][:, 1:], target_id, replacement_id,
+        tokenised_text[:, 1:], target_id, replacement_id,
     )
-    return text_inputs, text_targets
+    return text_inputs, attention_mask, text_targets
 
 
 def make_causal_mask(prefix_len:int, sequence_len:int, device:torch.device):
+    """ Image tokens (of prefix_len) are in the prefix, which can self attend.
+        Text tokens (of sequence_len) cannot attend to future tokens
+    """
     total_length = prefix_len + sequence_len
     square_causal_mask = torch.triu(
         torch.ones(
-            total_length, total_length, device=device,
+            total_length, total_length, device=device, dtype = torch.bool
         ), diagonal=1,
     )
     square_causal_mask[0:prefix_len, 0:prefix_len] = 0
     # mask = torch.cat([torch.ones((prefix_len, prefix_len), device=device), square_causal_mask], dim=1)
     return square_causal_mask
+
+def make_src_key_padding_mask(prefix_len, attention_mask, device:torch.device):
+    """
+    a binary mask of shape (N,S) indicating which elements within key to ignore for 
+    the purpose of attention (i.e. treat as “padding”). For unbatched query, 
+    shape should be (S). Binary and float masks are supported. For a binary 
+    mask, a True value indicates that the corresponding key value will be 
+    ignored for the purpose of attention. 
+
+    https://docs.pytorch.org/docs/stable/generated/torch.nn.MultiheadAttention.html
+    """
+    B, sequence_len = attention_mask.shape
+    attention_mask = attention_mask.to(dtype=torch.bool)
+    zeros_to_prepend = torch.zeros((B, prefix_len), dtype=torch.bool, device=device)
+    result = torch.cat((zeros_to_prepend, attention_mask), dim=1)
+    return result
 
 
 def process_padding(x: torch.Tensor, target: int, replacement:int) -> torch.Tensor:
@@ -132,7 +151,7 @@ class ImageCaptioner(nn.Module):
         )
         nn.init.normal_(self.positional_encodings, std=0.02)
 
-    def forward(self, image_inputs: list, text_inputs: dict) -> torch.Tensor:
+    def forward(self, image_inputs: torch.Tensor, text_inputs: torch.Tensor, text_attention_mask= None) -> torch.Tensor:
         """
         image_inputs (dict): key 'pixel_values' torch.Size([B, 3, 224, 224])
 
@@ -157,12 +176,20 @@ class ImageCaptioner(nn.Module):
         causal_mask = make_causal_mask(
             N_image_tokens, N_text_tokens, res.device,
         )
-        res = self.decoder(res, mask=causal_mask, src_key_padding_mask=None)
+        # If an attention mask is provided (during training)
+        if text_attention_mask is None:
+            src_key_padding_mask = None
+        else:
+            src_key_padding_mask = make_src_key_padding_mask(N_image_tokens, 
+                                                1 - text_attention_mask, 
+                                                text_attention_mask.device)
+        res = self.decoder(res, mask=causal_mask, 
+            src_key_padding_mask=src_key_padding_mask)
         # Return sequence tokens output and ignore the prefix
         out = self.language_head(res[:, N_image_tokens:])
         return out
 
-    def process_batch(self, image_inputs: list, text_inputs: list) -> tuple[dict, dict]:
+    def process_batch(self, image_inputs: list, text_inputs: list) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         # Returns dicts
         image_inputs = self.image_processor(
             images=image_inputs,
@@ -175,10 +202,14 @@ class ImageCaptioner(nn.Module):
         )
         # Clip if longer than the longest sequence length possible
         if text_inputs.input_ids.shape[1] > self.text_seq_len_max:
-            text_inputs['input_ids'] = text_inputs['input_ids'][:,:self.text_seq_len_max].clone()
-            text_inputs['attention_mask'] = text_inputs['attention_mask'][:,:self.text_seq_len_max].clone()
+            input_ids = text_inputs['input_ids'][:,:self.text_seq_len_max].clone()
+            attention_mask = text_inputs['attention_mask'][:,:self.text_seq_len_max].clone()
+        else: 
+            input_ids = text_inputs['input_ids']
+            attention_mask = text_inputs['attention_mask']
 
-        return image_inputs, text_inputs
+
+        return image_inputs['pixel_values'], input_ids, attention_mask
 
     def get_image_embeddings(self, vision_inputs: dict):
         # X_im is the result from the processor
@@ -192,10 +223,23 @@ class ImageCaptioner(nn.Module):
         # Shape is B, N_tokens, D_model = 512
         # this is also a possibility captioning_model.text_model(text_inputs.input_ids).last_hidden_state
         if type(text_inputs) is not torch.Tensor:
-            return self.text_model.text_model.embeddings.token_embedding(text_inputs['input_ids'])
+            return self.text_model.token_embedding(text_inputs['input_ids'])
         else:
-            return self.text_model.text_model.embeddings.token_embedding(text_inputs)
+            return self.text_model.token_embedding(text_inputs)
 
 
     def trainable_params(self):
         return filter(lambda p: p.requires_grad, self.parameters())
+
+    def forward_sequential(self, x: torch.Tensor):
+        # assert type(x) is torch.Tensor
+        generated = torch.tensor([[self.bos_id]], dtype=torch.int32, device=x.device)  
+        for _ in range(self.text_seq_len_max - generated.size(1)):
+            test_scores = self.forward(x, generated)    # assume outputs.logits [1, T, V]
+            # 3) Greedy pick at last position
+            next_token = torch.argmax(test_scores[:, -1, :], dim=-1, keepdim=True)  # [1,1]
+            # 4) Append and check EOS
+            generated = torch.cat([generated, next_token], dim=1)  # [1, T+1]
+            if next_token.item() == self.eos_id:
+                break
+        return generated[0]

training/train_captioner.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 import torch
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 import wandb
@@ -26,42 +26,46 @@ def calculate_accuracy(scores, y, pad_token_id=None):
         scores = scores[mask]
         y = y[mask]
     correct = torch.sum(scores.argmax(dim=-1) == y)
-    return correct/y.numel()
+    return correct/y.numel(), correct
 
 class Validator:
     def __init__(self, validation_dataloader: DataLoader, device: torch.device):
         self.valid_dl = validation_dataloader
         self.device = device
-        # self.pad_token_id = validation_dataloader.dataset.pad_token_id
-        # self.eos_token_id = validation_dataloader.dataset.eos_token_id
 
     def validate(self, model: torch.nn.Module, loss_fn: torch.nn.Module):
         total_correct = 0
         count = 0
         total_loss = 0.
+
         with torch.no_grad():
             for batch_idx, batch in enumerate(tqdm(self.valid_dl)):
                 x, y = batch
-                processed_images, processed_text = model.process_batch(x, y)
-                text_inputs, text_targets = get_text_inputs_and_targets(processed_text, 
-                                                                        model.eos_id, model.pad_id)
+
+                # THIS IS EXACTLY THE SAME CODE AS THE FORWARD -----------------
+
+                processed_images, processed_text, attention_mask = model.process_batch(x, y)
+                text_inputs, input_attention_mask, text_targets = get_text_inputs_and_targets(processed_text, 
+                                                                        attention_mask,
+                                                                        model.eos_id, 
+                                                                        model.pad_id)
                 processed_images = processed_images.to(self.device)
                 text_inputs = text_inputs.to(self.device)
                 text_targets = text_targets.to(self.device)
-
-                scores = model(processed_images, text_inputs)
-
+                input_attention_mask = input_attention_mask.to(self.device)
+                # Scores are (unnormalised) logits
+                scores = model(processed_images, text_inputs, input_attention_mask)
                 # Then do the loss
                 loss = loss_fn(scores.view(text_targets.numel(), -1), text_targets.view(-1))
+                #-------------------------------------------------------------------
+
                 total_loss += loss.item()
 
-                # TODO padding
-                mask = y != model.pad_id
-                scores = scores[mask]
-                target = target[mask]
-                
-                total_correct += torch.sum(scores.argmax(dim=-1) == target)
-                count += torch.numel(target)
+                # SAME CODE AS IN PER BATCH EVALUATION  -----------------
+                _, correct = calculate_accuracy(scores, text_targets, pad_token_id=model.pad_id)
+                #-------------------------------------------------------------------
+                total_correct += correct
+                count += torch.numel(text_targets)
 
         return total_loss / len(self.valid_dl), total_correct / count
 
@@ -116,78 +120,70 @@ class Trainer:
         for batch_idx, batch in enumerate(tqdm(self.train_dl)):
             # Zero your gradients for every batch!
             optimiser.zero_grad()
-            x, y = batch
             # y is a padded sequence
             # x shape B, D
-            # decoder input is indexed until the <end> token
+            x, y = batch
 
-            # Scores are (unnormalised) logits
-            processed_images, processed_text = model.process_batch(x, y)
-            text_inputs, text_targets = get_text_inputs_and_targets(processed_text, 
-                                                                    model.eos_id, model.pad_id)
-            # B, N_dec = text_targets.shape 
-
+            # CHANGE THIS-------------------------------------------------------
+            # Training forward code
+            processed_images, processed_text, attention_mask = model.process_batch(x, y)
+            text_inputs, input_attention_mask, text_targets = get_text_inputs_and_targets(processed_text, 
+                                                                    attention_mask,
+                                                                    model.eos_id, 
+                                                                    model.pad_id)
             processed_images = processed_images.to(self.device)
             text_inputs = text_inputs.to(self.device)
             text_targets = text_targets.to(self.device)
-
-            scores = model(processed_images, text_inputs)
+            input_attention_mask = input_attention_mask.to(self.device)
+            # Scores are (unnormalised) logits
+            scores = model(processed_images, text_inputs, input_attention_mask)
 
             # Then do the loss
             loss = loss_fn(scores.view(text_targets.numel(), -1), text_targets.view(-1))
+            #-------------------------------------------------------------------
             loss.backward()
             optimiser.step()
 
             # Gather data and report
             running_loss += loss.item()
             if batch_idx % batches_print_frequency == (batches_print_frequency - 1):
+
+                # CHANGE THIS---------------------------------------------------
+                # Logs and sanity checking code
                 logger.info(f'Correct seq:\t{model.text_tokenizer.decode(text_targets[0])}')
                 logger.info(
                     f'Predicted seq:\t{model.text_tokenizer.decode(scores.argmax(-1)[0])}')
                 
-                # Predict first token
-                # visualise_patched_input(x_im.squeeze().cpu(), None, 16)
                 with torch.no_grad():   
-                    generated = torch.tensor([[model.bos_id]], dtype=torch.int32, device=self.device)  
-                    for _ in range(model.text_seq_len_max - generated.size(1)):
-                        test_scores = model(processed_images[0:1], generated)    # assume outputs.logits [1, T, V]
-                        # 3) Greedy pick at last position
-                        next_token = torch.argmax(test_scores[:, -1, :], dim=-1, keepdim=True)  # [1,1]
-                        # 4) Append and check EOS
-                        generated = torch.cat([generated, next_token], dim=1)  # [1, T+1]
-                        if next_token.item() == model.eos_id:
-                            break
-                    print(generated)  
-                    print()     
-                    print(model.text_tokenizer.decode(generated[0]))     
-
-                    checkpoint = {
-                        'model_state_dict': model.state_dict(),
-                        'optimiser_state_dict': optimiser.state_dict(),
-                    }
-
-                    checkpoint_path = os.path.join(
-                        '/Users/kenton/projects/mlx-institute/transformer/checkpoints',
-                        f'{datetime.now().strftime("%Y%m%d_%H%M%S")}.pth',
-                    )
-                    # torch.save(checkpoint, checkpoint_path)  
-                    
-                
-
+                    # Predict first token
+                    generated = model.forward_sequential(processed_images[0:1])
+                    logger.info(
+                        f'Sequentially predicted seq:\t{model.text_tokenizer.decode(generated[0])}')
+  
                 # Calculate accuracy metric
-                accuracy = calculate_accuracy(scores, text_targets, pad_token_id=model.pad_id)
+                accuracy, _ = calculate_accuracy(scores, text_targets, pad_token_id=model.pad_id)
                 ppl = torch.exp(loss)
                 # loss per batch
                 last_loss = running_loss / batches_print_frequency
                 logger.info(
                     f'  For batch {batch_idx + 1}, the loss is {last_loss}, the accuracy is {accuracy}, the perplexity is {ppl}, ',
                 )
+                #-------------------------------------------------------------------
                 running_loss = 0.
+                
+                # In case you want to save every printed time
+                #    checkpoint = {
+                #     'model_state_dict': model.state_dict(),
+                #     'optimiser_state_dict': optimiser.state_dict(),
+                # }
+                # checkpoint_path = os.path.join(
+                #         '/Users/kenton/projects/mlx-institute/transformer/checkpoints',
+                #         f'{datetime.now().strftime("%Y%m%d_%H%M%S")}.pth',
+                #     )
+                #     # torch.save(checkpoint, checkpoint_path)  
 
         return last_loss, accuracy
 
-
-    
     def train(
         self,
         epochs: int,
@@ -201,8 +197,7 @@ class Trainer:
         log_to_wandb = config.get('log_to_wandb')
         log_locally = config.get('log_locally')
         checkpoint_folder = config.get(
-            'checkpoint_folder',
-            '/Users/kenton/projects/mlx-institute/transformer/checkpoints',
+            'checkpoint_folder'
         )
 
         if log_to_wandb:
@@ -299,6 +294,7 @@ if __name__ == '__main__':
         'log_locally': False,
         'log_to_wandb': log_to_wandb,
         'batches_print_frequency': 100,
+        'checkpoint_folder': 'checkpoints',
     }
 
     device = get_device()